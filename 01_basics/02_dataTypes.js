1e25375f1689340b6d0068ddf24d706b37926565
Ayush <111406614+AyushSonuu@users.noreply.github.com>
2023-09-06 18:57:30 +0000

    datypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using node js
+console.log(3+3);
+console.log("ayush");// code readability should be high
+let name = "aayush";
+let age = 18;
+let isLoggedin = true;
+
+
+// number =>2pow53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stabdalone value // object
+// undefined => // undefined
+// symbol => unique
+// object
+
+console.log(typeof null); //object
+console,log(typeof undefined); //undefined
